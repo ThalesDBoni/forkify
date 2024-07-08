db85f661ef1c21d36b9d589c5d6217ffbed42bbf
Thales De Boni <thalesdeboni5@gmail.com>
2024-07-08 11:35:20 -0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = 'a4873d62-8245-4b93-840e-95592449f620'
+export const MODAL_CLOSE_SEC = 2.5
